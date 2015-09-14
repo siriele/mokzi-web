2c96725435274b3f563b077ca6c939d15f03e0a0
Stanley Iriele <siriele2x3@gmail.com>
2015-09-13 17:57:04 -0700

    starting editor methodss

--- web/mokzi/scene/scene.dart
+++ web/mokzi/scene/scene.dart
@@ -2,12 +2,97 @@ library scene;
 
 import '../mokzi.dart';
 import '../event/event.dart';
+import '../character/character.dart';
+
+
+
+class SceneGraph {
+		Map<String, SceneNode> nodes;
+		int paths;
+		int entries;
+		// other stuff
+		SceneGraph(){
+			nodes = new Map<String, SceneNode>();
+			paths = 0;
+			entries = 0;
+		}
+
+		void detachParent(String target, String parent){
+			if (target == parent){
+				throw new StateError("You cannot detach yourself from your self");
+			}
+
+			if (nodes.containsKey(target) && nodes.containsKey(parent)){
+				nodes[target].detachParent(parent);
+				nodes[parent].detachChild(target);
+			}
+		}
+
+		void detachChild(String target, String child){
+			if (target == child){
+				throw new StateError("You cannot detach yourself from your self");
+			}
+
+			if (nodes.containsKey(target) && nodes.containsKey(child)){
+				nodes[target].detachChild(child);
+				nodes[child].detachParent(target);
+			}
+		}
+
+		void attachChild(String target, String child){
+			if (target == child){
+				throw new StateError("You cannot detach yourself from your self");
+			}
+			if (!nodes.containsKey(target)){
+				throw new StateError("That scene isn't in this graph");
+			}
+			if (nodes[target].parents.contains(child)){
+				throw new StateError("Created a cycle if this node is attached");
+			}
+			findParent(target, child);// if found cycle will be created
+		}
+
+		void attachParent(String target, String parent){
+			if (target == parent){
+				throw new StateError("You cannot detach yourself from your self");
+			}
+			if (!nodes.containsKey(target)){
+				throw new StateError("That scene isn't in this graph");
+			}
+
+			if (nodes[target].children.containsKey(parent)){
+				throw new StateError("Created a cycle if this node is attached");
+			}
+
+			findChild(target, parent);// if found cycle will be created
+		}
+
+		void findParent(String target, String parent){
+
+		}
+
+		// find ch
+		void findChild(String target, String child){
+
+		}
+		// maybe add some fancy move semantics
+}
+
 
 class SceneNode {
 	Map<String, String> children; //key: scene_id, value: transition_id
 	List<String> parents;
-	SceneNode():children = new Map<String, String>(), parents = new List<String>(){
+	SceneNode(){
+		children = new Map<String, String>();
+		parents = new List<String>();
+	}
 
+	void detachParent(String parent){
+		parents.remove(parent);
+	}
+
+	void detachChild(String child){
+		children.remove(child);
 	}
 	void addChild(String scene,[String st]){
 		 children[scene] = st;
@@ -29,11 +114,15 @@ class EventsRow {
 		return events.contains(eid);
 	}
 
-	void add(String eid){
-		if (events.contains(eid)){
-			return;
+	void removeEvent(String event){
+		if (events.contains(event)){
+			events.remove(event);
 		}
-		events.add(eid);
+	}
+	void addEvent(String event){
+		if (!events.contains(event)){
+			events.add(event);
+		}
 	}
 }
 
@@ -50,8 +139,10 @@ class Scene extends Savable {
 	Map<String,Object>meta;
 	List<EventsRow>queue;
 	List<String> events; 
+	List<String> characters;
 	Scene(String id, String this.story,[this.subtype]): super(id, SCENE){
 		events = new List<String>();
+		characters = new List<String>();
 	}
 
 	void loadAssets(){
@@ -77,6 +168,18 @@ class Scene extends Savable {
 		if (events.contains(e.id)){
 			events.remove(e.id);
 			e.removeScene(this.id);
+		}
+	}
+
+	void addCharacter(Character c){
+		if (!characters.contains(c.id)){
+			characters.add(c.id);
+		}
+	}
+
+	void removeCharacter(Character c){
+		if (characters.contains(c.id)){
+			characters.remove(c.id);
 		}
 	}
 }
--- web/mokzi/story/story.dart
+++ web/mokzi/story/story.dart
@@ -3,6 +3,7 @@ import '../mokzi.dart';
 import '../scene/scene.dart';
 import '../event/event.dart';
 import '../character/character.dart';
+import 'package:uuid/uuid.dart';
 const String STORY = "story";
 
 class Story extends Savable{
@@ -17,8 +18,17 @@ class Story extends Savable{
 	Map<String, Character> characters;
 	Map<String, EventTransition>etransitions;
 	Map<String, SceneTransition>stransitions;
+	Uuid uuid;
 	
 	Story(String id, [this.subtype, this.title]):super(id, STORY){
+		uuid = new Uuid();
+	 	genres = new List<String>();
+	 	events = new Map<String, Event>();
+	 	scenes = new Map<String, Scene>();
+	 	graph = new Map<String, SceneNode>();
+	 	characters = new Map<String, Character>();
+	 	etransitions = new Map<String, EventTransition>();
+	 	stransitions = new Map<String, SceneTransition>();
 	}
 
 	void loadMeta(){
@@ -27,18 +37,29 @@ class Story extends Savable{
 
 
     Event newEvent([String stype]){
-    	return null;
+    	String nid = uuid.v1();
+    	Event e = new Event(nid, stype);
+    	events[e.id] = e;
+    	return e;
     }
 
     Event updateEvent( Event object){
-    	return null;
+    	return object;
     }
 
     void deleteEvent(Event object){
-
+    	for (String s in object.scenes){
+    		if (scenes.containsKey(s)){
+    			scenes[s].removeEvent(object);
+    		}
+    	}
+    	events.remove(object);
     }
     Scene newScene([String stype]){
-    	return null;
+    	String nid = uuid.v1();
+    	Scene s = new Scene(nid, stype);
+    	scenes[s.id] = s;
+    	return s;
     }
 
     Scene updateScene( Scene object){
@@ -46,11 +67,18 @@ class Story extends Savable{
     }
 
     void deleteScene(Scene object){
-
+    	for (String e in object.events){
+    		if (events.containsKey(e)){
+    			events[e].removeScene(object.id);
+    		}
+    	}
     }
 
     Character newCharacter([String stype]){
-    	return null;
+    	String nid = uuid.v1();
+    	Character c = new Character(nid, stype);
+    	characters[c.id] = c;
+    	return c;
     }
 
     Character updateCharacter( Character object){
@@ -61,7 +89,10 @@ class Story extends Savable{
 
     }
     EventTransition newEventTransition([String stype]){
-    	return null;
+    	String nid = uuid.v1();
+    	EventTransition e = new EventTransition(nid, stype);
+    	etransitions[e.id] = e;
+    	return e;
     }
 
     EventTransition updateEventTransition( EventTransition object){
@@ -72,7 +103,10 @@ class Story extends Savable{
 
     }
     SceneTransition newSceneTransition([String stype]){
-    	return null;
+    	String nid = uuid.v1();
+    	SceneTransition s = new SceneTransition(nid, stype);
+    	stransitions[s.id] = s;
+    	return s;
     }
 
     SceneTransition updateSceneTransition( SceneTransition object){
@@ -81,6 +115,10 @@ class Story extends Savable{
 
     void deleteSceneTransition(SceneTransition object){
 
+    }
+
+    void addNodeToGraph(){
+    	
     }
 	
 }
